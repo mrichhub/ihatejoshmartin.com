dad226748e664865b4727945d09181c501c68722
Mike Richards <>
2025-08-03 16:08:40 -0400

    Updated to what's currently on ihatejoshmartin.com, except with dark mode support, and some css cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
-import { useState } from "react"
 import "./App.css"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
+import joshAwfulPic from "./assets/DontYouJustHateJoshMartin.jpeg"
 
 function App() {
-	const [count, setCount] = useState(0)
-
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
+			<img src={joshAwfulPic} className="josh-martin-awful-pic" alt="Don't you just hate Josh Martin?" />
+
+			<h1>I HATE Josh Martin</h1>
+
+			<p className="why-do-we-hate-josh-martin">
+				Why? Because he thinks he's SO good, and he is, and I HATE that!
 			</p>
 		</>
 	)
